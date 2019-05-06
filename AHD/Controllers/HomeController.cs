e7c05dd0fe89e24c8f67492d1e99f9fc407b27c1
gibs007 <35394788+gibs007@users.noreply.github.com>
2019-05-06 22:45:04 +0530

    init
    init

---
+++ AHD/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AHD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string userName = string.Empty;
+            string userSessionName = string.Empty;            
+            if (System.Web.HttpContext.Current != null &&
+                System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                userName = System.Web.HttpContext.Current.User.Identity.Name;
+                string userId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+                Session["isLoggedIn"] = true;
+                Session["userEmail"] = userName;
+                Session["userSessionId"] = userId;
+            }
+            else
+            {
+                Session["isLoggedIn"] = false;
+                Session["userEmail"] = null;
+                Session["userSessionId"] = null;
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
